c886252d347aeff3ed093239b9e107c7ae2ff88d
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-07-02 11:39:43 -0400

    double addition

---
+++ src/doubleaddition.java
@@ -0,0 +1,15 @@
+public class doubleaddition {
+
+    public static void main(String[] args) {
+        // Declare two double variables and assign values
+        double num1 = 3.5;
+        double num2 = 2.7;
+
+        // Calculate the sum of num1 and num2
+        double sum = num1 + num2;
+
+        // Print out the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+
+}
